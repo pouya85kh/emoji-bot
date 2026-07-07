966c8a56eb39f89815869eee9b357773fe73431f
pouya85kh <pouyaa387@gmail.com>
2026-07-07 13:57:20 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ EMOJI = {
     "link":        DECO_EMOJI_ID,  # 🔗 استخراج کد ایموجی
     "panel":       DECO_EMOJI_ID,  # 🖥 حساب من
     "help":        DECO_EMOJI_ID,  # ❓ راهنما
-    "mail":        5971889748615105853,  # ✉️ پشتیبانی
+    "mail":        DECO_EMOJI_ID,  # ✉️ پشتیبانی
     "gem":         DECO_EMOJI_ID,  # 💎 پشتیبانی هدر
     "bolt":        DECO_EMOJI_ID,  # ⚡ نکات/دسترسی‌ها
     "note":        DECO_EMOJI_ID,  # 📝 توضیحات
